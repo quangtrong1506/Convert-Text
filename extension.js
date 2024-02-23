86aa2f4cd816927d251e899a25b7081c133cb48c
Quang Trong <quangtrong1506@gmail.com>
2024-02-23 23:43:17 +0700

    update v0.0.2

--- extension.js
+++ extension.js
@@ -1,4 +1,11 @@
-const { clearText, removeVietnameseCharacters, QUICK_PICK_ITEM } = require('./helpers.js');
+const {
+    cleanText,
+    removeVietnameseCharacters,
+    QUICK_PICK_ITEM,
+    translateAPI,
+    LIST_LANGUAGE,
+} = require('./helpers.js');
+// import { QUICK_PICK_ITEM, cleanText, removeVietnameseCharacters, translateAPI } from './helpers.js';
 
 const vscode = require('vscode');
 /**
@@ -11,93 +18,188 @@ function activate(context) {
         async function () {
             const editor = vscode.window.activeTextEditor;
             if (!editor) return vscode.window.showWarningMessage('Please open a file');
-
             const selection = editor.selection;
-            if (selection && !selection.isEmpty) {
-                const selectionRange = new vscode.Range(
-                    selection.start.line,
-                    selection.start.character,
-                    selection.end.line,
-                    selection.end.character
-                );
-                const highlighted = editor.document.getText(selectionRange);
-                let newText = highlighted;
-                const qp = vscode.window.createQuickPick();
-                qp.items = QUICK_PICK_ITEM;
-                qp.show();
-                await qp.onDidChangeSelection((selectedItems) => {
-                    const select = selectedItems[0];
-                    switch (select.label) {
-                        //? camelCase
-                        case QUICK_PICK_ITEM[0].label:
-                            newText = clearText(newText);
-                            newText = newText
-                                .replace(/\s+[a-z]/g, (x) => x[1].toUpperCase())
-                                .replace(/\s/g, '')
-                                .replace(/\w/, (x) => x[0].toLowerCase());
-                            break;
-                        //? PascalCase
-                        case QUICK_PICK_ITEM[1].label:
-                            newText = clearText(newText);
-                            newText = newText
-                                .replace(/\s+[a-z]/g, (x) => x[1].toUpperCase())
-                                .replaceAll(/\s/g, '')
-                                .replace(/\w/, (x) => x[0].toUpperCase());
-                            break;
-
-                        //? CONSTANT
-                        case QUICK_PICK_ITEM[2].label:
-                            newText = clearText(newText);
-                            newText = newText.toUpperCase().replaceAll(/\s/g, '_');
-                            break;
-                        //? underscore
-                        case QUICK_PICK_ITEM[3].label:
-                            newText = clearText(newText);
-                            newText = newText.toLowerCase().replaceAll(/\s/g, '_');
-                            break;
-                        //? Link
-                        case QUICK_PICK_ITEM[4].label:
-                            newText = clearText(newText);
-                            newText = newText.replaceAll(/\s/g, '-');
-                            break;
-                        //? Uppercase
-                        case QUICK_PICK_ITEM[5].label:
-                            newText = newText.toUpperCase();
-                            break;
-                        //? lowercase
-                        case QUICK_PICK_ITEM[6].label:
-                            newText = newText.toLowerCase();
-                            break;
-
-                        case QUICK_PICK_ITEM[7].label:
-                            newText = newText.replace(/[^A-Za-z0-9\s]/g, '');
-                            break;
-                        case QUICK_PICK_ITEM[8].label:
-                            while (newText.match(/  /)) {
-                                newText = newText.replaceAll('  ', ' ');
-                            }
-                            break;
-                        case QUICK_PICK_ITEM[9].label:
-                            newText = removeVietnameseCharacters(newText);
-                            break;
-                        default:
-                            break;
-                    }
-                    qp.dispose();
-                    editor.edit((editBuilder) => {
-                        editBuilder.replace(selectionRange, newText);
-                    });
-                });
-            } else {
+            if (selection && !selection.isEmpty) showQuickPick();
+            else
                 vscode.window.showWarningMessage('Please select the text to convert!', {
                     modal: true,
                 });
-            }
         }
     );
     context.subscriptions.push(disposable);
 }
 
+const showQuickPick = async () => {
+    const qp = vscode.window.createQuickPick();
+    qp.items = QUICK_PICK_ITEM;
+    qp.show();
+    const editor = vscode.window.activeTextEditor;
+    const selectionRange = new vscode.Range(
+        editor.selection.start.line,
+        editor.selection.start.character,
+        editor.selection.end.line,
+        editor.selection.end.character
+    );
+    const highlighted = editor.document.getText(selectionRange);
+    let text = highlighted;
+    await qp.onDidChangeSelection(async (selectedItems) => {
+        const select = selectedItems[0];
+        let isSelectLanguage = false;
+        switch (select.label) {
+            //? camelCase
+            case QUICK_PICK_ITEM[0].label:
+                text = cleanText(text);
+                text = text
+                    .replace(/\s+[a-z]/g, (x) => x[1].toUpperCase())
+                    .replace(/\s/g, '')
+                    .replace(/\w/, (x) => x[0].toLowerCase());
+                break;
+            //? PascalCase
+            case QUICK_PICK_ITEM[1].label:
+                text = cleanText(text);
+                text = text
+                    .replace(/\s+[a-z]/g, (x) => x[1].toUpperCase())
+                    .replaceAll(/\s/g, '')
+                    .replace(/\w/, (x) => x[0].toUpperCase());
+                break;
+
+            //? CONSTANT
+            case QUICK_PICK_ITEM[2].label:
+                text = cleanText(text);
+                text = text.toUpperCase().replaceAll(/\s/g, '_');
+                break;
+            //? underscore
+            case QUICK_PICK_ITEM[3].label:
+                text = cleanText(text);
+                text = text.toLowerCase().replaceAll(/\s/g, '_');
+                break;
+            //? Link
+            case QUICK_PICK_ITEM[4].label:
+                text = cleanText(text);
+                text = text.replaceAll(/\s/g, '-');
+                break;
+            //? Uppercase
+            case QUICK_PICK_ITEM[5].label:
+                text = text.toUpperCase();
+                break;
+            //? lowercase
+            case QUICK_PICK_ITEM[6].label:
+                text = text.toLowerCase();
+                break;
+            case QUICK_PICK_ITEM[7].label:
+                text = text.replace(/[^A-Za-z0-9\s]/g, '');
+                break;
+            case QUICK_PICK_ITEM[8].label:
+                while (text.match(/  /)) {
+                    text = text.replaceAll('  ', ' ');
+                }
+                break;
+            case QUICK_PICK_ITEM[9].label:
+                text = removeVietnameseCharacters(text);
+                break;
+            //? vi >en
+            case QUICK_PICK_ITEM[10].label:
+                let tmp = await translate(text, 'vi', 'en');
+                if (tmp) text = tmp;
+                break;
+            //? en >vi
+            case QUICK_PICK_ITEM[11].label:
+                let tmp2 = await translate(text, 'en', 'vi');
+                if (tmp2) text = tmp2;
+                break;
+            //? translate
+            case QUICK_PICK_ITEM[12].label:
+                isSelectLanguage = true;
+
+            default:
+                console.log(select);
+                break;
+        }
+        qp.dispose();
+        if (isSelectLanguage) selectTranslationLanguage(text);
+        else
+            editor.edit((editBuilder) => {
+                editBuilder.replace(selectionRange, text);
+            });
+    });
+};
+const translate = async (text = 'Hello', from, to) => {
+    try {
+        console.log({ Text: text, from, to });
+        const response = await translateAPI({ Text: text, from, to });
+        return response[0].translations[0].text;
+    } catch (error) {
+        console.log('Lỗi');
+        if (error.name === 'FetchError')
+            vscode.window.showErrorMessage('Please check your internet connection again!');
+        else vscode.window.showErrorMessage('Internal error, please try again later!');
+        return null;
+    }
+};
+
+const selectTranslationLanguage = async (text) => {
+    const options = {
+        text: text,
+        from: 'en',
+        to: 'en',
+    };
+    const from = vscode.window.createQuickPick();
+    from.items = LIST_LANGUAGE.map((language) => {
+        let item = {
+            label: language.label,
+            description: `(${language.code})`,
+        };
+        return item;
+    });
+    from.title = 'Current language';
+    from.placeholder = 'Current language';
+    from.show();
+    await from.onDidChangeSelection(async (selectedItems) => {
+        const label = selectedItems[0].label;
+        options.from = LIST_LANGUAGE.find((item) => item.label === label).code;
+        from.dispose();
+        ///
+        const to = vscode.window.createQuickPick();
+        const listLanguageTo = LIST_LANGUAGE.filter((item) => item.code != options.to);
+        to.items = listLanguageTo.map((language) => {
+            let item = {
+                label: language.label,
+                description: `(${language.code})`,
+            };
+            return item;
+        });
+        to.title = 'Translate into language';
+        to.placeholder = 'Translate into language';
+        to.show();
+        await to.onDidChangeSelection(async (selectedItems) => {
+            const label = selectedItems[0].label;
+            options.to = LIST_LANGUAGE.find((item) => item.label === label).code;
+            to.dispose();
+            const editor = vscode.window.activeTextEditor;
+            const selectionRange = new vscode.Range(
+                editor.selection.start.line,
+                editor.selection.start.character,
+                editor.selection.end.line,
+                editor.selection.end.character
+            );
+            const highlighted = editor.document.getText(selectionRange);
+            let newText = await translate(highlighted, options.from, options.to);
+            if (newText) editTextInEditor(newText);
+        });
+    });
+};
+const editTextInEditor = (text) => {
+    const editor = vscode.window.activeTextEditor;
+    const selectionRange = new vscode.Range(
+        editor.selection.start.line,
+        editor.selection.start.character,
+        editor.selection.end.line,
+        editor.selection.end.character
+    );
+    editor.edit((editBuilder) => {
+        editBuilder.replace(selectionRange, text);
+    });
+};
 function deactivate() {}
 
 module.exports = {
--- helpers.js
+++ helpers.js
@@ -1,44 +1,192 @@
+const fetch = require('node-fetch');
+const LIST_LANGUAGE = [
+    { label: 'Afrikaans', code: 'af' },
+    { label: 'Albanian', code: 'sq' },
+    { label: 'Amharic', code: 'am' },
+    { label: 'Arabic', code: 'ar' },
+    { label: 'Armenian', code: 'hy' },
+    { label: 'Assamese', code: 'as' },
+    { label: 'Azerbaijani (Latin)', code: 'az' },
+    { label: 'Bangla', code: 'bn' },
+    { label: 'Bashkir', code: 'ba' },
+    { label: 'Basque', code: 'eu' },
+    { label: 'Bhojpuri', code: 'bho' },
+    { label: 'Bodo', code: 'brx' },
+    { label: 'Bosnian (Latin)', code: 'bs' },
+    { label: 'Bulgarian', code: 'bg' },
+    { label: 'Cantonese (Traditional)', code: 'yue' },
+    { label: 'Catalan', code: 'ca' },
+    { label: 'Chinese (Literary)', code: 'lzh' },
+    { label: 'Chinese Simplified', code: 'zh-Hans' },
+    { label: 'Chinese Traditional', code: 'zh-Hant' },
+    { label: 'chiShona', code: 'sn' },
+    { label: 'Croatian', code: 'hr' },
+    { label: 'Czech', code: 'cs' },
+    { label: 'Danish', code: 'da' },
+    { label: 'Dari', code: 'prs' },
+    { label: 'Divehi', code: 'dv' },
+    { label: 'Dogri', code: 'doi' },
+    { label: 'Dutch', code: 'nl' },
+    { label: 'English', code: 'en' },
+    { label: 'Estonian', code: 'et' },
+    { label: 'Faroese', code: 'fo' },
+    { label: 'Fijian', code: 'fj' },
+    { label: 'Filipino', code: 'fil' },
+    { label: 'Finnish', code: 'fi' },
+    { label: 'French', code: 'fr' },
+    { label: 'French (Canada)', code: 'fr-ca' },
+    { label: 'Galician', code: 'gl' },
+    { label: 'Georgian', code: 'ka' },
+    { label: 'German', code: 'de' },
+    { label: 'Greek', code: 'el' },
+    { label: 'Gujarati', code: 'gu' },
+    { label: 'Haitian Creole', code: 'ht' },
+    { label: 'Hausa', code: 'ha' },
+    { label: 'Hebrew', code: 'he' },
+    { label: 'Hindi', code: 'hi' },
+    { label: 'Hmong Daw (Latin)', code: 'mww' },
+    { label: 'Hungarian', code: 'hu' },
+    { label: 'Icelandic', code: 'is' },
+    { label: 'Igbo', code: 'ig' },
+    { label: 'Indonesian', code: 'id' },
+    { label: 'Inuinnaqtun', code: 'ikt' },
+    { label: 'Inuktitut', code: 'iu' },
+    { label: 'Inuktitut (Latin)', code: 'iu-Latn' },
+    { label: 'Irish', code: 'ga' },
+    { label: 'Italian', code: 'it' },
+    { label: 'Japanese', code: 'ja' },
+    { label: 'Kannada', code: 'kn' },
+    { label: 'Kashmiri', code: 'ks' },
+    { label: 'Kazakh', code: 'kk' },
+    { label: 'Khmer', code: 'km' },
+    { label: 'Kinyarwanda', code: 'rw' },
+    { label: 'Klingon', code: 'tlh-Latn' },
+    { label: 'Klingon (plqaD)', code: 'tlh-Piqd' },
+    { label: 'Konkani', code: 'gom' },
+    { label: 'Korean', code: 'ko' },
+    { label: 'Kurdish (Central)', code: 'ku' },
+    { label: 'Kurdish (Northern)', code: 'kmr' },
+    { label: 'Kyrgyz (Cyrillic)', code: 'ky' },
+    { label: 'Lao', code: 'lo' },
+    { label: 'Latvian', code: 'lv' },
+    { label: 'Lithuanian', code: 'lt' },
+    { label: 'Lingala', code: 'ln' },
+    { label: 'Lower Sorbian', code: 'dsb' },
+    { label: 'Luganda', code: 'lug' },
+    { label: 'Macedonian', code: 'mk' },
+    { label: 'Maithili', code: 'mai' },
+    { label: 'Malagasy', code: 'mg' },
+    { label: 'Malay (Latin)', code: 'ms' },
+    { label: 'Malayalam', code: 'ml' },
+    { label: 'Maltese', code: 'mt' },
+    { label: 'Maori', code: 'mi' },
+    { label: 'Marathi', code: 'mr' },
+    { label: 'Mongolian (Cyrillic)', code: 'mn-Cyrl' },
+    { label: 'Mongolian (Traditional)', code: 'mn-Mong' },
+    { label: 'Myanmar', code: 'my' },
+    { label: 'Nepali', code: 'ne' },
+    { label: 'Norwegian', code: 'nb' },
+    { label: 'Nyanja', code: 'nya' },
+    { label: 'Odia', code: 'or' },
+    { label: 'Pashto', code: 'ps' },
+    { label: 'Persian', code: 'fa' },
+    { label: 'Polish', code: 'pl' },
+    { label: 'Portuguese (Brazil)', code: 'pt' },
+    { label: 'Portuguese (Portugal)', code: 'pt-pt' },
+    { label: 'Punjabi', code: 'pa' },
+    { label: 'Queretaro Otomi', code: 'otq' },
+    { label: 'Romanian', code: 'ro' },
+    { label: 'Rundi', code: 'run' },
+    { label: 'Russian', code: 'ru' },
+    { label: 'Samoan (Latin)', code: 'sm' },
+    { label: 'Serbian (Cyrillic)', code: 'sr-Cyrl' },
+    { label: 'Serbian (Latin)', code: 'sr-Latn' },
+    { label: 'Sesotho', code: 'st' },
+    { label: 'Sesotho sa Leboa', code: 'nso' },
+    { label: 'Setswana', code: 'tn' },
+    { label: 'Sindhi', code: 'sd' },
+    { label: 'Sinhala', code: 'si' },
+    { label: 'Slovak', code: 'sk' },
+    { label: 'Slovenian', code: 'sl' },
+    { label: 'Somali (Arabic)', code: 'so' },
+    { label: 'Spanish', code: 'es' },
+    { label: 'Swahili (Latin)', code: 'sw' },
+    { label: 'Swedish', code: 'sv' },
+    { label: 'Tahitian', code: 'ty' },
+    { label: 'Tamil', code: 'ta' },
+    { label: 'Tatar (Latin)', code: 'tt' },
+    { label: 'Telugu', code: 'te' },
+    { label: 'Thai', code: 'th' },
+    { label: 'Tibetan', code: 'bo' },
+    { label: 'Tigrinya', code: 'ti' },
+    { label: 'Tongan', code: 'to' },
+    { label: 'Turkish', code: 'tr' },
+    { label: 'Turkmen (Latin)', code: 'tk' },
+    { label: 'Ukrainian', code: 'uk' },
+    { label: 'Upper Sorbian', code: 'hsb' },
+    { label: 'Urdu', code: 'ur' },
+    { label: 'Uyghur (Arabic)', code: 'ug' },
+    { label: 'Uzbek (Latin)', code: 'uz' },
+    { label: 'Vietnamese', code: 'vi' },
+    { label: 'Welsh', code: 'cy' },
+    { label: 'Xhosa', code: 'xh' },
+    { label: 'Yoruba', code: 'yo' },
+    { label: 'Yucatec Maya', code: 'yua' },
+    { label: 'Zulu', code: 'zu' },
+];
 const QUICK_PICK_ITEM = [
     {
         label: 'camelCase                                                       ',
-        description: 'EG: Visual Studio Code > visualStudioCode',
+        description: 'EG: Visual Studio Code   ⇒   visualStudioCode',
         picked: true,
     },
     {
         label: 'PascalCase                                                       ',
-        description: 'EG: Visual Studio Code > VisualStudioCode',
+        description: 'EG: Visual Studio Code   ⇒   VisualStudioCode',
     },
     {
         label: 'CONSTANT                                                      ',
-        description: 'EG: Visual Studio Code > VISUAL_STUDIO_CODE',
+        description: 'EG: Visual Studio Code   ⇒   VISUAL_STUDIO_CODE',
     },
     {
         label: 'underscore                                                      ',
-        description: 'EG: Visual Studio Code > visual_studio_code',
+        description: 'EG: Visual Studio Code   ⇒   visual_studio_code',
     },
     {
-        label: 'Dash                                                                  ',
-        description: 'EG: Visual Studio Code > Visual-Studio-Code',
+        label: 'Dash                                                                ',
+        description: 'EG: Visual Studio Code   ⇒   Visual-Studio-Code',
     },
     {
         label: 'UPPERCASE                                                     ',
-        description: 'EG: Visual Studio Code > VISUAL STUDIO CODE',
+        description: 'EG: Visual Studio Code   ⇒   VISUAL STUDIO CODE',
     },
     {
         label: 'lowercase                                                        ',
-        description: 'EG: Visual Studio Code > visual studio code',
+        description: 'EG: Visual Studio Code   ⇒   visual studio code',
     },
     {
         label: 'Delete special characters                                ',
-        description: 'Visual-Studio-Code > VisualStudioCode',
+        description: 'EG: Visual-Studio-Code   ⇒   VisualStudioCode',
     },
     {
         label: 'Beautify text                                                    ',
-        description: 'Visual   Studio Code > Visual Studio Code',
+        description: 'EG: Visual   Studio Code ⇒  Visual Studio Code',
     },
     {
         label: 'Remove Vietnamese characters                      ',
-        description: 'EG: Việt Nam > Viet Nam',
+        description: 'EG: Việt Nam ⇒ Viet Nam',
+    },
+    {
+        label: 'Translate: Vietnamese ⇒ English',
+        description: '',
+    },
+    {
+        label: 'Translate: English ⇒ Vietnamese',
+        description: '',
+    },
+    {
+        label: 'Translate: Many Languages',
+        description: '',
     },
 ];
 const removeVietnameseCharacters = (str) => {
@@ -56,7 +204,7 @@ const removeVietnameseCharacters = (str) => {
         .replace(/-+/g, '-');
     return str;
 };
-const clearText = (text) => {
+const cleanText = (text) => {
     text = text.replace(/[^A-Za-z0-9]/g, ' ').trim();
     while (text.match(/  /)) {
         text = text.replaceAll('  ', ' ');
@@ -64,5 +212,34 @@ const clearText = (text) => {
 
     return text;
 };
-module.exports = { clearText, removeVietnameseCharacters, QUICK_PICK_ITEM };
-// employee manager > employeeManager
+const translateAPI = async (
+    options = {
+        Text: 'Hello World',
+        from: 'en',
+        to: 'vi',
+    }
+) => {
+    const myHeaders = {
+        'Ocp-Apim-Subscription-Key': '63a9e9fdfaae40ca9fb5f816c4f3d540',
+        'Ocp-Apim-Subscription-Region': 'southeastasia',
+        'Content-type': 'application/json',
+    };
+    const response = await fetch(
+        'https://api-apc.cognitive.microsofttranslator.com/translate/' +
+            `?api-version=3.0&from=${options.from}&to=${options.to}`,
+        {
+            headers: myHeaders,
+            body: JSON.stringify([{ Text: options.Text }]),
+            method: 'POST',
+        }
+    );
+    const body = await response.json();
+    return body;
+};
+module.exports = {
+    LIST_LANGUAGE,
+    QUICK_PICK_ITEM,
+    cleanText,
+    removeVietnameseCharacters,
+    translateAPI,
+};
